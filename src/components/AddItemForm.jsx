8c11f6c827d51872d3d2b185dc194c33690c8c37
Ashirwad Pradhan <ashirwad.pradhan007@gmail.com>
2024-05-03 17:28:35 +0530

    added items to itemslist

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
 import Button from "./Button";
 
-function AddItemForm() {
+function AddItemForm({ setItems }) {
+  const [itemText, setItemText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!itemText) {
+      alert("Item cannot be empty");
+      return;
+    }
+
+    const newItem = {
+      id: new Date().getTime(),
+      name: itemText,
+      packed: false,
+    };
+
+    setItems((p) => [...p, newItem]);
+  };
+
+  const handleChange = (e) => {
+    setItemText(e.target.value);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Add an Item</h2>
-      <input type="text" />
+      <input type="text" value={itemText} onChange={handleChange} />
       <Button>Add to list</Button>
     </form>
   );
